77511f4d9ee1cb286d3442afe62fb0a889a37324
Abdulla Mian <woodworks87@gmail.com>
2018-05-02 10:06:56 -0500

    2

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -2,7 +2,6 @@ class StringCalculator
 
   def self.add(string)
     del = string.start_with?('//') ? /,|\n|#{string[2]}/ : /,|\n/
-    return 6 if string.start_with?('//')
-    string.split(del).reduce(0) { |acc, e| acc += e.to_i }
+    (string.start_with?('//') ? string[5..-1] : string).split(del).reduce(0) { |acc, e| acc += e.to_i }
   end
 end
